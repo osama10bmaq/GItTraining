e61a5f2ee9539412324bb13f09556f6b5f4d86b1
Omar <osama.bmaq@gmail.com>
2020-04-21 23:19:35 +0300

    getFeatureMethod commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
-
+    private static String currentBranch;
     public static void main(String[] args) {
         System.out.println("Local.Feature1");
+    }
+
+    public static String getCurrentBranch(){
+        return currentBranch;
     }
 }
